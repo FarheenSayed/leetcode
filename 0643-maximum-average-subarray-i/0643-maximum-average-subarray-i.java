3d4d4131a08dc23538fac64bea919088d43c055e
Farheen sayed Nalband <169084302+FarheenSayed@users.noreply.github.com>
2025-05-10 11:29:36 +0530

    Time: 5 ms (48.29%), Space: 56.2 MB (65.89%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        double sum = 0;
+
+        for (int i = 0; i< k; i++) {
+            sum += nums[i];
+        }
+
+        double maxSum = sum;
+
+        for(int i = k; i < n; i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum / k;
+        
+    }
+}
